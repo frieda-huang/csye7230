f2cc1411752e20193960a5788f6e03f9e44529a8
Frieda (Jingying) Huang <jingyingfhuang@gmail.com>
2024-10-13 06:32:33 -0400

    Add Alembic migration for created_at and last_modified fields; update env.py for database config

--- alembic/env.py
+++ alembic/env.py
@@ -1,13 +1,25 @@
+import os
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+from dotenv import load_dotenv
+from searchagent.models import Base
+
+load_dotenv()
+
+database_url = os.getenv("DATABASE_URL")
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+if database_url:
+    config.set_main_option("sqlalchemy.url", database_url)
+
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -18,7 +30,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -38,7 +50,7 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = config.get_main_option("sqlalchemy.url", database_url)
     context.configure(
         url=url,
         target_metadata=target_metadata,
