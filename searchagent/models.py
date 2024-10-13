732f51082645f16125744113f36fec8582f65915
Frieda (Jingying) Huang <jingyingfhuang@gmail.com>
2024-10-13 06:47:12 -0400

    Fixed circular foreign keys

--- searchagent/models.py
+++ searchagent/models.py
@@ -1,7 +1,6 @@
 from datetime import datetime
-
 from pgvector.sqlalchemy import Vector
-from sqlalchemy import DateTime, Float, ForeignKey, Integer, LargeBinary, String
+from sqlalchemy import ForeignKey, Integer, LargeBinary, String
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 """
@@ -21,13 +20,13 @@ class Page(Base):
     page_number: Mapped[int] = mapped_column(Integer, nullable=False)
     text_content: Mapped[str] = mapped_column(String)
     binary_content: Mapped[bytes] = mapped_column(LargeBinary)
-    last_modified: Mapped[datetime] = mapped_column(DateTime, nullable=False)
-    created_at: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+    last_modified: Mapped[str] = mapped_column(String, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(String, nullable=False)
 
-    doc_embedding_id: Mapped[int] = mapped_column(ForeignKey("doc_embedding.id"))
     file_id: Mapped[int] = mapped_column(ForeignKey("file.id"))
 
-    doc_embedding: Mapped[list["DocEmbedding"]] = relationship(
+    file: Mapped["File"] = relationship(back_populates="pages")
+    embeddings: Mapped[list["Embedding"]] = relationship(
         back_populates="page", cascade="all, delete-orphan"
     )
 
@@ -41,25 +40,25 @@ class File(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     filename: Mapped[str] = mapped_column(String, nullable=False)
     filepath: Mapped[str] = mapped_column(String, nullable=False)
-    filesize: Mapped[float] = mapped_column(Float, nullable=False)
     filetype: Mapped[str] = mapped_column(String, nullable=False)
     total_pages: Mapped[int] = mapped_column(Integer, nullable=False)
     summary: Mapped[str] = mapped_column(String)
-    last_modified: Mapped[datetime] = mapped_column(DateTime, nullable=False)
-    created_at: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+    last_modified: Mapped[datetime] = mapped_column(String, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(String, nullable=False)
 
-    page_id: Mapped[int] = mapped_column(ForeignKey("page.id"))
+    folder_id = mapped_column(ForeignKey("folder.id"), nullable=False)
 
     pages: Mapped[list[Page]] = relationship(
         back_populates="file", cascade="all, delete-orphan"
     )
+    folder: Mapped["Folder"] = relationship(back_populates="files")
 
     def __repr__(self) -> str:
         return f"<File(filename={self.filename}, filetype={self.filetype})>"
 
 
-class DocEmbedding(Base):
-    __tablename__ = "doc_embedding"
+class Embedding(Base):
+    __tablename__ = "embedding"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     vector_embedding: Mapped[list[Vector]] = mapped_column(Vector(128))
@@ -67,10 +66,11 @@ class DocEmbedding(Base):
     embedding_type: Mapped[str] = mapped_column(String, default="multi-vector")
 
     page_id: Mapped[int] = mapped_column(ForeignKey("page.id"))
-    file_id: Mapped[int] = mapped_column(ForeignKey("file.id"))
+
+    page: Mapped[Page] = relationship(back_populates="embeddings")
 
     def __repr__(self) -> str:
-        return f"<DocEmbedding(page_id={self.page_id}, file_id={self.file_id}, dim={self.dim})>"
+        return f"<Embedding(page_id={self.page_id}, file_id={self.file_id}, dim={self.dim})>"
 
 
 class Folder(Base):
@@ -79,13 +79,12 @@ class Folder(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     folder_name: Mapped[str] = mapped_column(String, nullable=False)
     folder_path: Mapped[str] = mapped_column(String, nullable=False)
-    created_at: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(String, nullable=False)
 
     user_id = mapped_column(ForeignKey("user.id"), nullable=False)
-    file_id = mapped_column(ForeignKey("file.id"), nullable=False)
 
-    user: Mapped["User"] = relationship(back_populates="folder")
-    file: Mapped[list[File]] = relationship(
+    user: Mapped["User"] = relationship(back_populates="folders")
+    files: Mapped[list[File]] = relationship(
         back_populates="folder", cascade="all, delete-orphan"
     )
 
@@ -101,8 +100,10 @@ class Query(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     text: Mapped[str] = mapped_column(String, nullable=False)
     vector_embedding: Mapped[Vector] = mapped_column(Vector)
-    created_at: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(String, nullable=False)
     user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+
+    user: Mapped["User"] = relationship(back_populates="queries")
 
     def __repr__(self) -> str:
         return f"<Query(text={self.text})>"
@@ -114,10 +115,10 @@ class User(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String, nullable=False, unique=True)
     password_hash: Mapped[str] = mapped_column(String, nullable=False)
-    created_at: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(String, nullable=False)
 
-    query: Mapped[list[Query]] = relationship(back_populates="user")
-    folder: Mapped[list[Folder]] = relationship(
+    queries: Mapped[list[Query]] = relationship(back_populates="user")
+    folders: Mapped[list[Folder]] = relationship(
         back_populates="user", cascade="all, delete-orphan"
     )
 
