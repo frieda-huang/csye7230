b69ec6298f3cf8af26369cf97f95fb63dc2dbe91
Frieda (Jingying) Huang <jingyingfhuang@gmail.com>
2024-10-24 01:50:12 -0400

    Refactored to use enum for agent names

--- searchagent/agents/api.py
+++ searchagent/agents/api.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from functools import partial
 
 from searchagent.agents.factory import AgentFactory
@@ -10,42 +11,53 @@ from searchagent.agents.tools.pdf_search import PDFSearchTool
 input_dir = "."
 
 
+class Agent(Enum):
+    TRIAGE = "TriageAgent"
+    RETRIEVAL = "FileRetrievalAgent"
+    SYNC = "SyncAgent"
+    INDEX = "IndexAgent"
+    EMBED = "EmbedAgent"
+
+    def __str__(self):
+        return self.value
+
+
 def transfer_back_to_triage(factory: AgentFactory):
-    return factory.get_agent("TriageAgent")
+    return factory.get_agent(Agent.TRIAGE)
 
 
 def transfer_to_file_retrieval(factory: AgentFactory):
-    return factory.get_agent("FileRetrievalAgent")
+    return factory.get_agent(Agent.RETRIEVAL)
 
 
 def transfer_to_sync(factory: AgentFactory):
-    return factory.get_agent("SyncAgent")
+    return factory.get_agent(Agent.SYNC)
 
 
 def transfer_to_index(factory: AgentFactory):
-    return factory.get_agent("IndexAgent")
+    return factory.get_agent(Agent.INDEX)
 
 
 def transfer_to_embed(factory: AgentFactory):
-    return factory.get_agent("EmbedAgent")
+    return factory.get_agent(Agent.EMBED)
 
 
 async def create_agents(factory: AgentFactory):
     """TriageAgent routes incoming queries to the appropriate specialized agents or tools"""
     await factory.create_and_register_agent(
-        name="TriageAgent", functions=[partial(transfer_to_file_retrieval, factory)]
+        name=Agent.TRIAGE, functions=[partial(transfer_to_file_retrieval, factory)]
     )
 
     """Query Vector DB, i.e. pgvector"""
     await factory.create_and_register_agent(
-        name="FileRetrievalAgent",
+        name=Agent.RETRIEVAL,
         functions=[partial(transfer_back_to_triage, factory)],
         custom_tools=[PDFSearchTool(input_dir=input_dir)],
     )
 
     """Monitor file system changes and manage sync between local and db"""
     await factory.create_and_register_agent(
-        name="SyncAgent",
+        name=Agent.SYNC,
         functions=[
             partial(transfer_back_to_triage, factory),
             partial(transfer_to_index, factory),
@@ -56,14 +68,14 @@ async def create_agents(factory: AgentFactory):
 
     """Handle indexing of the new or updated files using HNSW"""
     await factory.create_and_register_agent(
-        name="IndexAgent",
+        name=Agent.INDEX,
         functions=[partial(transfer_back_to_triage, factory)],
         custom_tools=[Index()],
     )
 
     """Generate embeddings for new or updated files using ColPali"""
     await factory.create_and_register_agent(
-        name="EmbedAgent",
+        name=Agent.EMBED,
         functions=[partial(transfer_back_to_triage, factory)],
         custom_tools=[Embed()],
     )
