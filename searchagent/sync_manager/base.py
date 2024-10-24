75a16ca51dbee8dec8c0b33bb50a0ac28c7c22a2
Frieda (Jingying) Huang <jingyingfhuang@gmail.com>
2024-10-24 01:02:24 -0400

    Replaced threading with asyncio.create_task() for non-blocking execution

--- searchagent/sync_manager/base.py
+++ searchagent/sync_manager/base.py
@@ -1,38 +1,38 @@
-import threading
-import time
+import asyncio
 
 from searchagent.sync_manager.db import insert_data
-from searchagent.sync_manager.file_change_detector import Watcher, FileChangeEventList
+from searchagent.sync_manager.file_change_detector import FileChangeEventList, Watcher
 
-INTERVAL = 300  # 5 MINS
-
-
-def periodic_insert(event_list: FileChangeEventList):
-    while True:
-        time.sleep(INTERVAL)
-        if event_list:
-            insert_data(event_list)
-            event_list.clear()
+INTERVAL = 3  # 5 MINS
 
 
-def monitor(input_dir: str):
+class Monitor:
+    def __init__(self, input_dir: str):
+        self.input_dir = input_dir
+        self.event_list: FileChangeEventList = []
 
-    # TODO: Periodically delete old entries
-    # TODO: Allow more than one dir
-    event_list: FileChangeEventList = []
-
-    w = Watcher(input_dir, event_list)
-    w.run()
-
-    try:
-        # Start the periodic insertion in a separate thread
-        insert_thread = threading.Thread(target=periodic_insert(event_list))
-        insert_thread.daemon = True  # Ensure thread closes when the main program exits
-        insert_thread.start()
-
-        # Keep the main program running
+    async def periodic_insert(self, event_list: FileChangeEventList):
         while True:
-            time.sleep(1)  # This keeps the main thread alive
-    finally:
-        w.observer.stop()
-        w.observer.join()
+            await asyncio.sleep(INTERVAL)
+            if event_list:
+                insert_data(event_list)
+                event_list.clear()
+
+    async def run(self):
+        # TODO: Periodically delete old entries
+        # TODO: Allow more than one dir
+        event_list: FileChangeEventList = []
+
+        w = Watcher(self.input_dir, event_list)
+
+        try:
+            # Start the periodic insertion as an asyncio task
+            asyncio.create_task(self.periodic_insert(event_list))
+            await asyncio.to_thread(w.run)
+
+            # Keep the main program running
+            while True:
+                await asyncio.sleep(1)  # This keeps the main thread alive
+        finally:
+            w.observer.stop()
+            w.observer.join()
