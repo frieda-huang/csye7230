fa56e5fc5742af6e3b355bb92c7610f5e4271d79
Frieda (Jingying) Huang <jingyingfhuang@gmail.com>
2024-10-27 15:49:17 -0400

    Moved routine tools into a separate folder

---
+++ searchagent/agents/tools/routines/file_monitor.py
@@ -0,0 +1,25 @@
+from typing import Dict
+
+from llama_stack_client.types.tool_param_definition_param import (
+    ToolParamDefinitionParam,
+)
+from searchagent.agents.common.custom_tools import SingleMessageCustomTool
+from searchagent.sync_manager.base import Monitor
+
+
+class FileMonitor(SingleMessageCustomTool):
+    def __init__(self, input_dir=".") -> None:
+        self.input_dir = input_dir
+        self.monitor = Monitor(input_dir)
+
+    def get_name(self) -> str:
+        return "file_monitor"
+
+    def get_description(self) -> str:
+        return "Continuously monitor your files for any changes"
+
+    def get_params_definition(self) -> Dict[str, ToolParamDefinitionParam]:
+        return NotImplementedError
+
+    async def run_impl(self):
+        return await self.monitor.run()
---
+++ searchagent/agents/tools/routines/index.py
@@ -0,0 +1,22 @@
+from typing import Dict
+from llama_stack_client.types.tool_param_definition_param import (
+    ToolParamDefinitionParam,
+)
+from searchagent.agents.common.custom_tools import SingleMessageCustomTool
+
+
+class Index(SingleMessageCustomTool):
+    def __init__(self):
+        pass
+
+    def get_name(self) -> str:
+        return "index"
+
+    def get_description(self) -> str:
+        return "Index new or updated files"
+
+    def get_params_definition(self) -> Dict[str, ToolParamDefinitionParam]:
+        return NotImplementedError
+
+    async def run_impl(self):
+        return NotImplementedError
