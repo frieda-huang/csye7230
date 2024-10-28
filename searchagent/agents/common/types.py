b6e6703e7fd1eae07fbe79417c4618833c472814
Frieda (Jingying) Huang <jingyingfhuang@gmail.com>
2024-10-28 11:13:44 -0400

    Hacky way to bypass the serialization error in custom tool's get_params_definition()

--- searchagent/agents/common/types.py
+++ searchagent/agents/common/types.py
@@ -1,5 +1,9 @@
 from enum import Enum
 
+from llama_stack_client.types.tool_param_definition_param import (
+    ToolParamDefinitionParam,
+)
+
 
 class AgentType(Enum):
     triage_agent = "triage_agent"
@@ -10,3 +14,13 @@ class AgentType(Enum):
 
     def __str__(self):
         return self.value
+
+
+# FIXME: This is a hack to bypass the JSON serialization error in custom tool's get_params_definition()
+custom_tool_handoff_agent_params = {
+    "query": ToolParamDefinitionParam(
+        param_type="str",
+        description="Determines which specialized agent or custom tool should handle the task",
+        required=True,
+    )
+}
