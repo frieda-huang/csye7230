9f094d21e172e5682f4274e62e910be36b1648a4
Frieda (Jingying) Huang <jingyingfhuang@gmail.com>
2024-10-30 17:18:14 -0400

    Cleaned up on handoffs logic

--- searchagent/agents/common/execute_with_custom_tools.py
+++ searchagent/agents/common/execute_with_custom_tools.py
@@ -5,112 +5,76 @@
 # the root directory of this source tree.
 
 
-import json
 from typing import AsyncGenerator, List, Optional, Union
 
-from llama_stack_client.types import Attachment, UserMessage
-from llama_stack_client.types.agent_create_params import AgentConfig
+from llama_stack_client.types import Attachment, ToolResponseMessage, UserMessage
 from loguru import logger
-from searchagent.agents.common.custom_tools import (
-    CustomTool,
-    Message,
-    ToolResponseMessage,
-)
+from searchagent.agents.app_context import client
+from searchagent.agents.common.custom_tools import CustomTool, Message
+from searchagent.agents.common.types import AgentWithCustomToolExecutor
 
 
-class AgentWithCustomToolExecutor:
-    def __init__(
-        self,
-        name: str,
-        agent_id: str,
-        session_id: str,
-        agent_config: AgentConfig,
-        custom_tools: List[CustomTool],
-    ):
-        self.name = (name,)
-        self.agent_id = agent_id
-        self.session_id = session_id
-        self.agent_config = agent_config
-        self.custom_tools = custom_tools
+async def execute_turn(
+    agent: AgentWithCustomToolExecutor,
+    messages: List[Union[UserMessage, ToolResponseMessage]],
+    attachments: Optional[List[Attachment]] = None,
+    stream: bool = True,
+) -> AsyncGenerator:
+    # from searchagent.agents.app_context import client
 
-    def to_dict(self):
-        return {
-            "name": self.name,
-            "agent_id": self.agent_id,
-            "session_id": self.session_id,
-            "agent_config": self.agent_config,
-            "custom_tools": [ct.to_dict() for ct in self.custom_tools],
-        }
+    tools_dict = {t.get_name(): t for t in agent.custom_tools}
 
-    @staticmethod
-    def from_dict(dict: dict):
-        return AgentWithCustomToolExecutor(**dict)
+    current_messages = messages.copy()
 
-    async def execute_turn(
-        self,
-        messages: List[Union[UserMessage, ToolResponseMessage]],
-        attachments: Optional[List[Attachment]] = None,
-        stream: bool = True,
-    ) -> AsyncGenerator:
-        from searchagent.agents.app_context import client
-        from searchagent.agents.tools.routines import PDFSearchTool
-
-        tools_dict = {t.get_name(): t for t in self.custom_tools}
-
-        current_messages = messages.copy()
-
-        while True:
-            response = client.agents.turn.create(
-                agent_id=self.agent_id,
-                session_id=self.session_id,
-                messages=current_messages,
-                attachments=attachments,
-                stream=True,
-            )
-            turn = None
-            for chunk in response:
-                if chunk.event.payload.event_type != "turn_complete":
-                    yield chunk
-                else:
-                    turn = chunk.event.payload.turn
-
-            message = turn.output_message
-
-            if len(message.tool_calls) == 0:
+    while True:
+        response = client.agents.turn.create(
+            agent_id=agent.agent_id,
+            session_id=agent.session_id,
+            messages=current_messages,
+            attachments=attachments,
+            stream=True,
+        )
+        turn = None
+        for chunk in response:
+            if chunk.event.payload.event_type != "turn_complete":
                 yield chunk
-                return
+            else:
+                turn = chunk.event.payload.turn
 
-            if message.stop_reason == "out_of_tokens":
-                yield chunk
-                return
+        message = turn.output_message
 
-            for tool_call in message.tool_calls:
-                if tool_call.tool_name not in tools_dict:
-                    m = ToolResponseMessage(
-                        call_id=tool_call.call_id,
-                        tool_name=tool_call.tool_name,
-                        content=f"Unknown tool `{tool_call.tool_name}` was called. Try again with something else",
-                        role="ipython",
-                    )
-                    next_message = m
-                else:
-                    tool = tools_dict[tool_call.tool_name]
-                    result_messages = await execute_custom_tool(tool, message)
+        if len(message.tool_calls) == 0:
+            yield chunk
+            return
 
-                    for result_message in result_messages:
-                        custom_tool = json.loads(result_message.content)
+        if message.stop_reason == "out_of_tokens":
+            yield chunk
+            return
 
-                        if custom_tool["name"] == "pdf_search":
-                            result = await PDFSearchTool.from_dict(
-                                custom_tool, input_dir="."
-                            ).run_impl("find the page about limitation of colpali")
-                            logger.debug(result)
+        for tool_call in message.tool_calls:
+            if tool_call.tool_name not in tools_dict:
+                m = ToolResponseMessage(
+                    call_id=tool_call.call_id,
+                    tool_name=tool_call.tool_name,
+                    content=f"Unknown tool `{tool_call.tool_name}` was called. Try again with something else",
+                    role="ipython",
+                )
+                next_message = m
+            else:
+                tool = tools_dict[tool_call.tool_name]
+                result_messages = await execute_custom_tool(tool, message)
 
-                        next_message = result_message
+                if type(result_messages) is AgentWithCustomToolExecutor:
+                    current_agent = result_messages
+                    logger.debug(current_agent)
 
-                        yield next_message
-                        current_messages = [next_message]
+                next_message = result_messages[0]
+
+            yield next_message
+            current_messages = [next_message]
 
 
-async def execute_custom_tool(tool: CustomTool, message: Message) -> List[Message]:
+async def execute_custom_tool(
+    tool: CustomTool, message: Message
+) -> Union[AgentWithCustomToolExecutor, ToolResponseMessage]:
     return await tool.run([message])
--- searchagent/agents/common/single_message_custom_tool.py
+++ searchagent/agents/common/single_message_custom_tool.py
@@ -1,13 +1,10 @@
-import json
 from abc import abstractmethod
-from typing import List
+from typing import List, Union
 
 from llama_stack_client.types import CompletionMessage, ToolResponseMessage
 from loguru import logger
 from searchagent.agents.common.custom_tools import CustomTool
-from searchagent.agents.common.execute_with_custom_tools import (
-    AgentWithCustomToolExecutor,
-)
+from searchagent.agents.common.types import AgentWithCustomToolExecutor
 
 
 class SingleMessageCustomTool(CustomTool):
@@ -17,7 +14,9 @@ class SingleMessageCustomTool(CustomTool):
     allow for the tool be called by the model and the necessary plumbing.
     """
 
-    async def run(self, messages: List[CompletionMessage]) -> List[ToolResponseMessage]:
+    async def run(
+        self, messages: List[CompletionMessage]
+    ) -> Union[AgentWithCustomToolExecutor, ToolResponseMessage]:
 
         assert len(messages) == 1, "Expected single message"
 
@@ -25,28 +24,21 @@ class SingleMessageCustomTool(CustomTool):
 
         tool_call = message.tool_calls[0]
 
-        result = []
-
         try:
             response = await self.run_impl(**tool_call.arguments)
-            custom_tools = AgentWithCustomToolExecutor(**response).custom_tools
 
-            for custom_tool in custom_tools:
-                response_str = json.dumps(custom_tool, ensure_ascii=False)
-
-                message = ToolResponseMessage(
+            if "agent_id" in response:
+                return AgentWithCustomToolExecutor(**response)
+            else:
+                return ToolResponseMessage(
                     call_id=tool_call.call_id,
                     tool_name=tool_call.tool_name,
-                    content=response_str,
+                    content=response,
                     role="ipython",
                 )
 
-                result.append(message)
-
         except Exception as e:
             logger.error(f"Error when running tool: {e}")
-
-        return result
 
     @abstractmethod
     async def run_impl(self, *args, **kwargs):
