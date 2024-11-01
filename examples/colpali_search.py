8564bdbdd9efb98910e6812eab7e77d9b9e3ae3b
Frieda (Jingying) Huang <jingyingfhuang@gmail.com>
2024-10-31 23:54:11 -0400

    Updated example to run inference via fastapi

--- examples/colpali_search.py
+++ examples/colpali_search.py
@@ -1,3 +1,6 @@
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
 from searchagent.colpali.base import ColPaliRag
 from searchagent.utils import project_paths
 
@@ -5,6 +8,22 @@ pdfs_dir = project_paths.PDF_DIR
 single_file_dir = project_paths.SINGLE_FILE_DIR
 embeddings_filepath = f"{project_paths}/embeddings_metadata.json"
 
-# Page 4 of the paper Attention Is All You Need
 rag = ColPaliRag(input_dir=pdfs_dir, store_locally=False)
-rag.search(query="Find a page on 'Scaled Dot-Product Attention'")
+app = FastAPI()
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    _ = rag.model
+    _ = rag.processor
+
+
+@app.get("/")
+async def root():
+    return {"message": "ColPali Search API", "docs": "/docs", "health": "/health"}
+
+
+@app.post("/search")
+async def search(query: str):
+    response = rag.search(query=query)
+    return {"response": response}
