d7211f40b9995bbfc0d3ba396597be89e09ed5f1
Frieda (Jingying) Huang <jingyingfhuang@gmail.com>
2024-11-04 17:54:16 -0500

    Added yield to save log file upon shutting down

--- examples/colpali_search.py
+++ examples/colpali_search.py
@@ -2,22 +2,27 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 from searchagent.colpali.base import ColPaliRag
-from searchagent.utils import project_paths
 from searchagent.db_connection import enable_pgvector_extension
+from searchagent.utils import project_paths
 
 pdfs_dir = project_paths.PDF_DIR
 single_file_dir = project_paths.SINGLE_FILE_DIR
 embeddings_filepath = f"{project_paths}/embeddings_metadata.json"
 
 rag = ColPaliRag(input_dir=pdfs_dir, store_locally=False)
-app = FastAPI()
+_ = rag.model
+_ = rag.processor
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    _ = rag.model
-    _ = rag.processor
     await enable_pgvector_extension()
+    print("Starting up")
+    yield
+    print("Shutting down")
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/")
