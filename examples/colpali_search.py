7b4eb98fe9a1af8a516c36d8da5487de87d46b0b
Frieda (Jingying) Huang <jingyingfhuang@gmail.com>
2024-11-14 23:23:57 -0500

    Added another endpoint for benchmarking

--- examples/colpali_search.py
+++ examples/colpali_search.py
@@ -3,12 +3,14 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from searchagent.colpali.base import ColPaliRag
 from searchagent.db_connection import enable_pgvector_extension
+from searchagent.ragmetrics.metrics import average_recall, fetch_dataset_column
 from searchagent.utils import project_paths
 
 pdfs_dir = project_paths.PDF_DIR
 single_file_dir = project_paths.SINGLE_FILE_DIR
 
-rag = ColPaliRag(benchmark=True)
+
+rag = ColPaliRag(benchmark=False)
 _ = rag.model
 _ = rag.processor
 
@@ -33,3 +35,15 @@ async def root():
 async def search(query: str):
     response = await rag.search(query=query)
     return {"response": response}
+
+
+@app.post("/benchmark")
+async def benchmark():
+    k = 10
+    pages = fetch_dataset_column(column_name="page")
+    queries = fetch_dataset_column(column_name="query")
+    actual_pages = [int(page_number) for page_number in pages]
+
+    score = await average_recall(rag, queries, actual_pages, k)
+
+    return {"score": score}
