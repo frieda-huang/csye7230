f2120e556b25fc35e1b957fc02f671f998623792
Frieda (Jingying) Huang <jingyingfhuang@gmail.com>
2024-11-23 20:57:10 -0500

    Added conftest.py

---
+++ colpali-search/tests/repository/conftest.py
@@ -0,0 +1,115 @@
+import numpy as np
+import pytest_asyncio
+from colpali_search.models import (
+    Base,
+    Embedding,
+    File,
+    FlattenedEmbedding,
+    IndexingStrategy,
+    Page,
+    Query,
+    User,
+)
+from colpali_search.utils import get_now
+from pgvector.psycopg import register_vector_async
+from sqlalchemy import event, text
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+
+
+@pytest_asyncio.fixture(scope="function")
+async def async_session():
+    async_engine = create_async_engine(
+        "postgresql+psycopg://localhost/colpalisearch_test",
+        isolation_level="AUTOCOMMIT",
+    )
+
+    @event.listens_for(async_engine.sync_engine, "connect")
+    def connect(dbapi_connection, connection_record):
+        dbapi_connection.run_async(register_vector_async)
+
+    async_session_maker = async_sessionmaker(bind=async_engine, expire_on_commit=False)
+
+    async with async_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.execute(text("CREATE EXTENSION IF NOT EXISTS vector"))
+        await conn.run_sync(Base.metadata.create_all)
+
+    async with async_session_maker() as session:
+        try:
+            yield session
+        finally:
+            await session.rollback()
+            await session.close()
+
+
+@pytest_asyncio.fixture(scope="function")
+async def seed(async_session):
+    async with async_session.begin():
+        VEC_DIM = 128
+        vectors = [
+            np.array([1.0] * VEC_DIM, dtype=np.float16),
+            np.array([2.0] * VEC_DIM, dtype=np.float16),
+        ]
+
+        user = User(
+            id=1,
+            email="test@gmail.com",
+            password_hash="123",
+            created_at=get_now(),
+        )
+        async_session.add(user)
+
+        file = File(
+            id=1,
+            filename="test.pdf",
+            filetype="pdf",
+            total_pages=1,
+            last_modified=get_now(),
+            created_at=get_now(),
+        )
+        async_session.add(file)
+
+        page = Page(
+            id=1,
+            page_number=1,
+            last_modified=get_now(),
+            created_at=get_now(),
+            file=file,
+        )
+        async_session.add(page)
+
+        embedding = Embedding(
+            id=1,
+            vector_embedding=vectors,
+            last_modified=get_now(),
+            created_at=get_now(),
+            page=page,
+        )
+        async_session.add(embedding)
+
+        flattened_embedding = FlattenedEmbedding(
+            id=1,
+            vector_embedding=vectors[0],
+            last_modified=get_now(),
+            created_at=get_now(),
+            embedding=embedding,
+        )
+        async_session.add(flattened_embedding)
+
+        query = Query(
+            id=1,
+            text="test query",
+            vector_embedding=vectors,
+            created_at=get_now(),
+            user=user,
+        )
+        async_session.add(query)
+
+        indexing_strategy = IndexingStrategy(
+            id=1,
+            strategy_name="ExactMaxSim",
+            created_at=get_now(),
+        )
+        async_session.add(indexing_strategy)
+
+        await async_session.commit()
--- colpali-search/tests/repository/test_sqlalchemy.py
+++ colpali-search/tests/repository/test_sqlalchemy.py
@@ -1,6 +1,6 @@
 import numpy as np
+import pytest
 from colpali_search.models import (
-    Base,
     Embedding,
     File,
     FlattenedEmbedding,
@@ -9,179 +9,75 @@ from colpali_search.models import (
     Query,
     User,
 )
-from colpali_search.utils import get_now
-from pgvector.psycopg import register_vector
-from sqlalchemy import create_engine, event, select, text
-from sqlalchemy.orm import Session
-
-engine = create_engine("postgresql+psycopg://localhost/colpalisearch_test")
-
-with Session(engine) as session:
-    session.execute(text("CREATE EXTENSION IF NOT EXISTS vector"))
-    session.commit()
-
-array_engine = create_engine("postgresql+psycopg://localhost/colpalisearch_test")
+from sqlalchemy import select
 
 
-@event.listens_for(array_engine, "connect")
-def connect(dbapi_connection, connection_record):
-    register_vector(dbapi_connection)
+@pytest.mark.asyncio
+async def test_file_orm(seed, async_session):
+    file_stmt = select(File)
+    page_stmt = select(Page)
+    file = await async_session.scalar(file_stmt)
+    page = await async_session.scalar(page_stmt)
+
+    assert file.filename == "test.pdf"
+    assert file.pages[0].id == page.id
 
 
-Base.metadata.drop_all(engine)
-Base.metadata.create_all(engine)
+@pytest.mark.asyncio
+async def test_page_orm(seed, async_session):
+    page_stmt = select(Page)
+    page = await async_session.scalar(page_stmt)
 
-VEC_DIM = 128
-vectors = [
-    np.array([1.0] * VEC_DIM, dtype=np.float16),
-    np.array([2.0] * VEC_DIM, dtype=np.float16),
-]
+    assert page.file_id == 1
+    assert page.page_number == 1
 
 
-def create_data():
-    session = Session(engine)
+@pytest.mark.asyncio
+async def test_embedding_orm(seed, async_session):
+    embedding_stmt = select(Embedding)
+    page_stmt = select(Page)
+    embedding = await async_session.scalar(embedding_stmt)
+    page = await async_session.scalar(page_stmt)
 
-    user = User(
-        id=1,
-        email="test@gmail.com",
-        password_hash="123",
-        created_at=get_now(),
+    assert embedding.id == page.id
+    assert np.array_equal(
+        embedding.vector_embedding[0].tolist(), np.array([1.0] * 128, dtype=np.float16)
     )
-    session.add(user)
 
-    file = File(
-        id=1,
-        filename="test.pdf",
-        filetype="pdf",
-        total_pages=1,
-        last_modified=get_now(),
-        created_at=get_now(),
+
+@pytest.mark.asyncio
+async def test_flattened_embedding_orm(seed, async_session):
+    stmt = select(FlattenedEmbedding)
+    flattened_embedding = await async_session.scalar(stmt)
+
+    assert np.array_equal(
+        flattened_embedding.vector_embedding.tolist(),
+        np.array([1.0] * 128, dtype=np.float16),
     )
-    session.add(file)
 
-    page = Page(
-        id=1,
-        page_number=1,
-        last_modified=get_now(),
-        created_at=get_now(),
-        file=file,
+
+@pytest.mark.asyncio
+async def test_query_orm(seed, async_session):
+    stmt = select(Query)
+    query = await async_session.scalar(stmt)
+
+    assert np.array_equal(
+        query.vector_embedding[1].tolist(),
+        np.array([2.0] * 128, dtype=np.float16),
     )
-    session.add(page)
-
-    embedding = Embedding(
-        id=1,
-        vector_embedding=vectors,
-        last_modified=get_now(),
-        created_at=get_now(),
-        page=page,
-    )
-    session.add(embedding)
-
-    flattened_embedding = FlattenedEmbedding(
-        id=1,
-        vector_embedding=vectors[0],
-        last_modified=get_now(),
-        created_at=get_now(),
-        embedding=embedding,
-    )
-    session.add(flattened_embedding)
-
-    query = Query(
-        id=1,
-        text="test query",
-        vector_embedding=vectors,
-        created_at=get_now(),
-        user=user,
-    )
-    session.add(query)
-
-    indexing_strategy = IndexingStrategy(
-        id=1,
-        strategy_name="ExactMaxSim",
-        created_at=get_now(),
-    )
-    session.add(indexing_strategy)
-
-    session.commit()
 
 
-class TestSqlalchemy:
-    def setup_method(self, test_method):
-        with Session(engine) as session:
-            models = [
-                Query,
-                FlattenedEmbedding,
-                Embedding,
-                Page,
-                File,
-                User,
-                IndexingStrategy,
-            ]
-            for each in models:
-                session.query(each).delete()
-            session.commit()
+@pytest.mark.asyncio
+async def test_user_orm(seed, async_session):
+    stmt = select(User)
+    user = await async_session.scalar(stmt)
 
-    def test_file_orm(self):
-        create_data()
-        with Session(engine) as session:
-            file_stmt = select(File)
-            page_stmt = select(Page)
-            file = session.scalar(file_stmt)
-            page = session.scalar(page_stmt)
+    assert user.email == "test@gmail.com"
 
-            assert file.filename == "test.pdf"
-            assert file.pages[0].id == page.id
 
-    def test_page_orm(self):
-        create_data()
-        with Session(engine) as session:
-            page_stmt = select(Page)
-            page = session.scalar(page_stmt)
+@pytest.mark.asyncio
+async def test_indexing_strategy(seed, async_session):
+    stmt = select(IndexingStrategy)
+    indexing_strategy = await async_session.scalar(stmt)
 
-            assert page.file_id == 1
-            assert page.page_number == 1
-
-    def test_embedding_orm(self):
-        create_data()
-        with Session(array_engine) as session:
-            embedding_stmt = select(Embedding)
-            page_stmt = select(Page)
-            embedding = session.scalar(embedding_stmt)
-            page = session.scalar(page_stmt)
-
-            assert embedding.id == page.id
-            assert np.array_equal(embedding.vector_embedding[0].to_list(), vectors[0])
-
-    def test_flattened_embedding_orm(self):
-        create_data()
-        with Session(array_engine) as session:
-            stmt = select(FlattenedEmbedding)
-            flattened_embedding = session.scalar(stmt)
-
-            assert np.array_equal(
-                flattened_embedding.vector_embedding.to_list(), vectors[0]
-            )
-
-    def test_query_orm(self):
-        create_data()
-        with Session(array_engine) as session:
-            stmt = select(Query)
-            query = session.scalar(stmt)
-
-            assert np.array_equal(query.vector_embedding[1].to_list(), vectors[1])
-
-    def test_user_orm(self):
-        create_data()
-        with Session(engine) as session:
-            stmt = select(User)
-            user = session.scalar(stmt)
-
-            assert user.email == "test@gmail.com"
-
-    def test_indexing_strategy(self):
-        create_data()
-        with Session(engine) as session:
-            stmt = select(IndexingStrategy)
-            indexing_strategy = session.scalar(stmt)
-
-            assert indexing_strategy.strategy_name == "ExactMaxSim"
+    assert indexing_strategy.strategy_name == "ExactMaxSim"
